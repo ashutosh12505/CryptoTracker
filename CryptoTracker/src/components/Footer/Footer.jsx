8066485c4db5e62e5c851ac0f0d27ba71639f34b
Ashutosh <ashutosh12505@gmail.com>
2025-06-24 14:38:34 +0530

    Initial commit

---
+++ CryptoTracker/src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Footer.css'
+
+export default function Footer(){
+    return(
+        <div className="footer">
+            <p>Copyright &copy; 2024, Cryptoplace - All Rights Reserved</p>
+        </div>
+    )
+}
